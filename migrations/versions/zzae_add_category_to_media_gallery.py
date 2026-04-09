b2746391e044edec50c883dddfd457b4ed4f6a2f
James Kamau <jamesnk5773@gmail.com>
2026-04-09 12:40:03 +0300

    Shorten migration revision ID to fit alembic version column (32 char limit)

--- migrations/versions/zzae_add_category_to_media_gallery.py
+++ migrations/versions/zzae_add_category_to_media_gallery.py
@@ -1,6 +1,6 @@
 """add category field to media_gallery for user-defined grouping
 
-Revision ID: zzae_add_category_to_media_gallery
+Revision ID: zzae_add_category
 Revises: zzad_add_event_id
 Create Date: 2026-04-09 10:15:00.000000
 
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'zzae_add_category_to_media_gallery'
+revision = 'zzae_add_category'
 down_revision = 'zzad_add_event_id'
 branch_labels = None
 depends_on = None
